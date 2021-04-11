fc0f841b51ec844a80fb1bde227a742624458026
leandromarten1 <leandromarten1@gmail.com>
2021-04-11 01:17:17 -0300

    Add favorite logic

--- front-end/src/components/Card/Card.jsx
+++ front-end/src/components/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Box, Image, Flex } from '@chakra-ui/react';
@@ -6,10 +6,27 @@ import { StarIcon } from '@chakra-ui/icons';
 
 const Card = ({ info }) => {
   const isMarked = false;
+  const [star, setStar] = useState(
+    JSON.parse(localStorage.getItem('favorites')) || [],
+  );
   const { id, name, thumbnail, title } = info;
+
+  const addFavorite = ({ id }) => {
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    if (!favorites.some((fav) => fav === id)) {
+      favorites.push(id);
+    } else {
+      favorites.splice(favorites.indexOf(id), 1);
+    }
+
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+    setStar(favorites);
+  };
+
   return (
     <Box overflow='hidden' borderWidth='1px' bg='#fff'>
       <Link to={name ? `/characters/${id}` : `/comics/${id}`}>
+        {console.log('favoritos state:', star)}
         <Image
           src={`${thumbnail.path}.${thumbnail.extension}`}
           alt={`Image ${name}`}
@@ -19,7 +36,12 @@ const Card = ({ info }) => {
       <Box bg='red' h={1} w='100%'></Box>
       <Flex p='6' directon='row' justify='space-between' align='center'>
         <Link to='#'>{name || title}</Link>
-        <StarIcon h='5' w='5' color={isMarked ? 'red' : '#ccc'} />
+        <StarIcon
+          h='5'
+          w='5'
+          color={star.includes(id) ? 'red' : '#ccc'}
+          onClick={() => addFavorite(info)}
+        />
       </Flex>
     </Box>
   );
