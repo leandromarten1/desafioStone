98d4dc353359b6ee2a836a8d607c519331c5ce73
leandromarten1 <leandromarten1@gmail.com>
2021-04-11 10:44:00 -0300

    Sequelize Init

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Running server on port ${PORT}!`));
